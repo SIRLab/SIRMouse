0af7e2e91fa047b3d5b34747eb01d3bb6796fa5a
Rafael Almeida <rafaelalmeidatk@gmail.com>
2016-10-19 23:53:02 -0200

    New controls and fixed movement

--- sirmouse/sirmouse.ino
+++ sirmouse/sirmouse.ino
@@ -18,6 +18,9 @@ int ch2Value = 0;
 int motorsE[2] = { 5, 6 };
 int motorsM[2] = { 4, 7 };
 
+const int SIDE = 0;
+const int MOVEMENT = 1;
+
 //--------------------------------------------------
 // Moves
 
@@ -44,40 +47,43 @@ int currentSpeed = 0;
 // Movement changed
 
 bool changed = false;
+unsigned int sideTick = 0;
 
 //--------------------------------------------------
 
 void motorDirection(int moveId) {
-  int e[2];
+  int state, type;
   switch (moveId) {
     case FRONT:
-      e[0] = HIGH;
-      e[1] = HIGH;
+      state = LOW;
+      type = MOVEMENT;
       break;
     case BACK:
-      e[0] = LOW;
-      e[1] = LOW;
+      state = HIGH;
+      type = MOVEMENT;
       break;
     case LEFT:
-      e[0] = LOW;
-      e[1] = HIGH;
+      state = HIGH;
+      type = SIDE;
       break;
     case RIGHT:
-      e[0] = HIGH;
-      e[1] = LOW;
+      state = LOW;
+      type = SIDE;
       break;
   }
-  for (int i = 0; i < 2; i++) {
-    digitalWrite(motorsM[i], e[i]);
-  }
+  digitalWrite(motorsM[type], state);
 }
 
 void moveRobot(int moveId) {
-  Serial.print("move robot: ");
+  Serial.print("Move robot: ");
   Serial.println(moveId);
   motorDirection(moveId);
-  for (int i = 0; i < 2; i++) {
-    analogWrite(motorsE[i], 255);
+
+  int index = moveId == FRONT || moveId == BACK ? MOVEMENT : SIDE;
+  analogWrite(motorsE[index], 255);
+  
+  if (moveId == LEFT || moveId == RIGHT) {
+    sideTick = millis() + 250;
   }
 }
 
@@ -92,19 +98,28 @@ void changedState() {
 }
 
 bool gotMove(int idMove) {
-  if (idMove == FRONT && ch1Value > 1700) {
+  if (idMove == FRONT && ch1Value != 0 && ch1Value > 1700) {
     return true;
-  } else if (idMove == BACK && ch1Value < 1100) {
+  } else if (idMove == BACK && ch1Value != 0 && ch1Value < 1100) {
     return true;
   }
-  if (idMove == RIGHT && ch2Value > 1700) {
+  if (idMove == RIGHT && ch2Value != 0 && ch2Value > 1700) {
     return true;
-  } else if (idMove == LEFT && ch2Value  < 1400) {
+  } else if (idMove == LEFT && ch2Value != 0 && ch2Value  < 1100) {
     return true;
   }
 
   return false;
 }
+
+// FIRST: ESQUERDA | DIREITA
+// SECOND: FRENTE | TRAS
+
+// LOW - LOW: FRENTE (DIREITA)
+// LOW - HIGH: TRAS (DIREITA)
+
+// HIGH - HIGH: TRAS (ESQUERDA)
+// HIGH - LOW: FRENTE (ESQUERDA
 
 void setup()
 {
@@ -112,14 +127,14 @@ void setup()
   for (int i = 0; i < 2; i++) {
     pinMode(motorsM[i], OUTPUT);
   }
-  digitalWrite(motorsM[0], LOW);
-  digitalWrite(motorsM[1], HIGH);
-  
+  digitalWrite(motorsM[0], HIGH);
+  digitalWrite(motorsM[1], LOW);
+
   pinMode(rcCh1Pin, INPUT);
   pinMode(rcCh2Pin, INPUT);
 
   for (int i = 0; i < 2; i++) {
-    analogWrite(motorsE[i], 255);
+    analogWrite(motorsE[i], 0);
   }
 }
 
@@ -131,8 +146,10 @@ void loop()
   Serial.print(ch1Value);
   Serial.print('\t');
   Serial.print("Channel #2: ");
-  Serial.println(ch2Value);
-  
+  Serial.print(ch2Value);
+  Serial.print('\t');
+  Serial.println(sideTick);
+
   if (gotMove(FRONT)) {
     moveRobot(FRONT);
     changedState();
@@ -143,16 +160,17 @@ void loop()
 
   if (gotMove(RIGHT)) {
     moveRobot(RIGHT);
-    changedState();
   } else if (gotMove(LEFT)) {
     moveRobot(LEFT);
-    changedState();
   }
-  
+
+  if (sideTick != 0 && millis() > sideTick) {
+    analogWrite(motorsE[SIDE], 0);
+    sideTick = 0;
+  }
+
   if (changed) {
-    for (int i = 0; i < 2; i++) {
-      analogWrite(motorsE[i], 0);
-    }
+    analogWrite(motorsE[MOVEMENT], 0);
     changed = false;
   }
-}
+} 
